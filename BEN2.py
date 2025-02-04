48e27c1bb52cfbdaefd56abb404b9bf17763d1d4
Prama <pramadevelopment@gmail.com>
2025-02-04 13:52:42 -0600

    Update BEN2.py

--- BEN2.py
+++ BEN2.py
@@ -28,7 +28,6 @@ def set_random_seed(seed):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-set_random_seed(9)
 
 
 torch.set_float32_matmul_precision('highest')
